fc9eda7c3593542f82b91c6283eb5572d5ed6c64
soju6jan <cybersol@naver.com>
2020-08-15 13:31:48 +0900

    1

--- logic_user.py
+++ logic_user.py
@@ -275,10 +275,16 @@ class LogicUser(object):
                     logger.error(traceback.format_exc())
             else:
                 def func():
-                    ret = RcloneTool.do_action(ModelSetting.get('rclone_path'), ModelSetting.get('rclone_config_path'),  'download', '', folder_id, '', '', my_remote_path, 'real', folder_id_encrypted=True, listener=None)
-
-                    msg = u'모두 완료되었습니다.'
-                    socketio.emit("command_modal_add_text", str(msg), namespace='/framework', broadcast=True)
+                    for i in range(1, 11):
+                        ret = RcloneTool.do_action(ModelSetting.get('rclone_path'), ModelSetting.get('rclone_config_path'),  'download', '', folder_id, '', '', my_remote_path, 'real', folder_id_encrypted=True, listener=None)
+                        if ret['percent'] == 0:
+                            msg = u'아직 토렌트 파일을 받지 못했습니다. 30초 후 다시 시도합니다. (%s/20)' % i
+                            socketio.emit("command_modal_add_text", str(msg), namespace='/framework', broadcast=True)
+                            time.sleep(30)
+                        else:
+                            msg = u'모두 완료되었습니다.'
+                            socketio.emit("command_modal_add_text", str(msg), namespace='/framework', broadcast=True)
+                            break
 
                 thread = threading.Thread(target=func, args=())
                 thread.setDaemon(True)
